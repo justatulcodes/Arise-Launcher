143400bbafbf6e602a40205abc523f5728601da9
Atul Kumar <atul.k.work@gmail.com>
2025-10-03 16:38:45 +0530

    removed app drawer bottom nav tab and updated app install listener

--- app/src/main/java/com/expeknow/ariselauncher/MainActivity.kt
+++ app/src/main/java/com/expeknow/ariselauncher/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.expeknow.ariselauncher
 
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,13 +14,25 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.expeknow.ariselauncher.ui.navigation.AppNavigation
 import com.expeknow.ariselauncher.ui.theme.AriseLauncherTheme
+import com.expeknow.ariselauncher.utils.PackageChangeReceiver
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val packageReceiver = PackageChangeReceiver()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val filter = IntentFilter().apply {
+            addAction(Intent.ACTION_PACKAGE_ADDED)
+            addAction(Intent.ACTION_PACKAGE_REMOVED)
+            addAction(Intent.ACTION_PACKAGE_REPLACED)
+            addDataScheme("package")
+        }
+        registerReceiver(packageReceiver, filter)
+        Log.d("PackageReceiver", "Package receiver registered")
 
         setContent {
             AriseLauncherTheme {
--- app/src/main/java/com/expeknow/ariselauncher/ui/navigation/Screen.kt
+++ app/src/main/java/com/expeknow/ariselauncher/ui/navigation/Screen.kt
@@ -1,13 +1,11 @@
 package com.expeknow.ariselauncher.ui.navigation
 
 sealed class Screen(val route: String) {
-    data object Home : Screen("home")
-    data object AppList : Screen("applist")
     data object Settings : Screen("settings")
     data object Focus : Screen("focus")
     data object Points : Screen("points")
     data object Drive : Screen("drive")
-    data object Apps : Screen("apps")
+//    data object Apps : Screen("apps")
     data object Ctrl : Screen("ctrl")
     data class TaskDetails(val id: String) : Screen("taskdetails/{id}") {
         companion object {
