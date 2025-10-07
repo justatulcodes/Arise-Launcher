bc55bb0ee0da36df57b60030596cff473643a240
Atul Kumar <atul.k.work@gmail.com>
2025-10-07 23:54:47 +0530

    code cleanup

--- app/src/main/java/com/expeknow/ariselauncher/ui/navigation/Screen.kt
+++ app/src/main/java/com/expeknow/ariselauncher/ui/navigation/Screen.kt
@@ -5,7 +5,6 @@ sealed class Screen(val route: String) {
     data object Focus : Screen("focus")
     data object Points : Screen("points")
     data object Drive : Screen("drive")
-//    data object Apps : Screen("apps")
     data object Ctrl : Screen("ctrl")
     data class TaskDetails(val id: String) : Screen("taskdetails/{id}") {
         companion object {
