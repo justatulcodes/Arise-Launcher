5ff636cc763762b8177264eb69cf28d17fc7dc88
Atul Kumar <atul.k.work@gmail.com>
2025-11-15 17:51:00 +0530

    added db migrations for app launch count trackers

--- app/src/main/java/com/expeknow/ariselauncher/data/database/AriseLauncherDatabase.kt
+++ app/src/main/java/com/expeknow/ariselauncher/data/database/AriseLauncherDatabase.kt
@@ -5,9 +5,8 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import androidx.room.migration.Migration
-import androidx.sqlite.db.SupportSQLiteDatabase
 import com.expeknow.ariselauncher.data.database.AriseDatabaseMigrations.MIGRATION_2_3
+import com.expeknow.ariselauncher.data.database.AriseDatabaseMigrations.MIGRATION_3_4
 import com.expeknow.ariselauncher.data.database.dao.AppInfoDao
 import com.expeknow.ariselauncher.data.database.dao.DriveItemDao
 import com.expeknow.ariselauncher.data.database.dao.PointsLogDao
@@ -22,7 +21,7 @@ import com.expeknow.ariselauncher.data.model.TaskLink
 
 @Database(
     entities = [Task::class, TaskLink::class, PointsLog::class, AppInfo::class, DriveItem::class],
-    version = 3,
+    version = 4,
     exportSchema = true
 )
 @TypeConverters(ModelTypeConverters::class)
@@ -45,7 +44,7 @@ abstract class AriseLauncherDatabase : RoomDatabase() {
                     AriseLauncherDatabase::class.java,
                     "arise_launcher_database"
                 )
-                    .addMigrations(MIGRATION_2_3)
+                    .addMigrations(MIGRATION_2_3, MIGRATION_3_4)
                     .build()
                 INSTANCE = instance
                 instance
